0862f477324dd4eb1c65f89d37dcae6cdc32299c
topsweg <67249845+topsweg@users.noreply.github.com>
2026-09-25 13:53:38 -0400

    Fix Derby optimization criteria definitions

--- games/derby_dash_v1/game_optimization.py
+++ games/derby_dash_v1/game_optimization.py
@@ -8,13 +8,13 @@ class OptimizationSetup:
             "base":{
                 "conditions":{
                     "freegame":ConstructConditions(rtp=0.342,hr=280,search_conditions={"symbol":"scatter"}).return_dict(),
-                    "basegame":ConstructConditions(rtp=0.618).return_dict(),
+                    "basegame":ConstructConditions(rtp=0.618,av_win=1.0).return_dict(),
                 },
                 "scaling":{},
                 "parameters":ConstructParameters(num_show=5000,num_per_fence=10000,min_m2m=4,max_m2m=8,pmb_rtp=1.0,sim_trials=5000,test_spins=[50,100,200],test_weights=[0.3,0.4,0.3],score_type="rtp").return_dict(),
             },
             "bonus":{
-                "conditions":{"freegame":ConstructConditions(rtp=0.96,hr="x").return_dict()},
+                "conditions":{"freegame":ConstructConditions(rtp=0.96,av_win=96.0).return_dict()},
                 "scaling":{},
                 "parameters":ConstructParameters(num_show=5000,num_per_fence=10000,min_m2m=4,max_m2m=8,pmb_rtp=1.0,sim_trials=5000,test_spins=[10,20,50],test_weights=[0.6,0.2,0.2],score_type="rtp").return_dict(),
             },
