d0c5213df297909eeb08179940a01dd4c2cbf86f
topsweg <67249845+topsweg@users.noreply.github.com>
2026-09-25 11:03:45 -0400

    Derby Dash: implement run.py

---
+++ games/derby_dash_v1/run.py
@@ -0,0 +1,14 @@
+"""Derby Dash SDK simulation runner."""
+from gamestate import GameState
+from game_config import GameConfig
+from src.state.run_sims import create_books
+from src.write_data.write_configs import generate_configs
+from utils.rgs_verification import execute_all_tests
+
+if __name__=="__main__":
+    config=GameConfig()
+    gamestate=GameState(config)
+    num_sim_args={"base":1000,"bonus":1000}
+    create_books(gamestate,config,num_sim_args,batching_size=500,num_threads=1,compression=False,profiling=False)
+    generate_configs(gamestate)
+    execute_all_tests(config)
