0b19caccef0799161488d4683424caf784de9ebf
topsweg <67249845+topsweg@users.noreply.github.com>
2026-09-25 22:29:28 -0400

    Derby Dash: align base fence residual probability with 1-in-280 feature rate

--- games/derby_dash_v1/game_optimization.py
+++ games/derby_dash_v1/game_optimization.py
@@ -8,7 +8,7 @@ class OptimizationSetup:
             "base":{
                 "conditions":{
                     "freegame":ConstructConditions(rtp=0.342,hr=280).return_dict(),
-                    "basegame":ConstructConditions(rtp=0.618,av_win=1.0).return_dict(),
+                    "basegame":ConstructConditions(rtp=0.618,hr="x",av_win=0.620214337).return_dict(),
                 },
                 "scaling":{},
                 "parameters":ConstructParameters(num_show=5000,num_per_fence=10000,min_m2m=4,max_m2m=8,pmb_rtp=1.0,sim_trials=5000,test_spins=[50,100,200],test_weights=[0.3,0.4,0.3],score_type="rtp").return_dict(),
